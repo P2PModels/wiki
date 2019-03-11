53fe96fb4e2d074e8961627bca9bd7431459ebee
Jordi <jordi.burguet.castell@gmail.com>
2019-03-11 17:56:18 +0100

    + history.js, show edit history panel

---
+++ app/components/history.js
@@ -0,0 +1,75 @@
+import React from 'react'
+
+import { Button, Text, IconAdd, theme } from '@aragon/ui'
+import { IconWrapper } from './ui-components'
+import styled from 'styled-components'
+
+const History = ({ pages, selectedRevision = 'last', viewDiff, showRevision }) => (
+  <Main>
+    <div>
+      <h1>
+        <Text color={theme.textSecondary} smallcaps>
+          History
+        </Text>
+        <Button onClick={viewDiff} mode="text">
+          <IconWrapper className="accent">
+            <IconAdd />
+          </IconWrapper>
+          <span className="action-label accent">Show differences</span>
+        </Button>
+      </h1>
+      <ul>
+        {Object.keys(pages).map(page => (
+          <li key={page}>
+            <Button mode="text" wide onClick={e => showRevision(page)}>
+              {selectedRevision === page ? <strong>{page}</strong> : page}
+            </Button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  </Main>
+)
+
+const Main = styled.aside`
+  flex-shrink: 0;
+  flex-grow: 0;
+  width: 260px;
+  margin-left: 30px;
+  min-height: 100%;
+  .accent {
+    color: ${theme.accent};
+  }
+  h1 {
+    margin-bottom: 15px;
+    color: ${theme.textSecondary};
+    text-transform: lowercase;
+    line-height: 30px;
+    font-variant: small-caps;
+    font-weight: 600;
+    font-size: 16px;
+    border-bottom: 1px solid ${theme.contentBorder};
+    button {
+      margin-top: -5px;
+      float: right;
+      span {
+        display: inline;
+      }
+      .action-label {
+        vertical-align: 5px;
+      }
+    }
+  }
+  ul {
+    list-style-type: none;
+    button {
+      text-align: left;
+      &:hover {
+        color: ${theme.selectionForeground};
+        background-color: ${theme.infoBackground};
+      }
+    }
+  }
+`
+
+export default History
