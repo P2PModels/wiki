b7637f22375838f4f9af48ff13fe51a5e383b5a8
David Llop <931684+sembrestels@users.noreply.github.com>
2019-05-16 12:49:39 +0200

    Use hooks in ipfs checker component

--- app/src/components/ipfs.js
+++ app/src/components/ipfs.js
@@ -1,57 +1,53 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { ipfs } from '../lib/ipfs-util'
 import { SafeLink } from '@aragon/ui'
 import { Trans } from 'react-i18next'
 
-class IpfsIsConnected extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = { connected: true }
-  }
+function IpfsIsConnected() {
+  /* Peers status:
+   * true: uninitialized
+   * false: not connected
+   * int: number of peers
+   */
+  const [peers, setPeersStatus] = useState(true)
 
-  componentDidMount() {
+  useEffect(() => {
     const checkPeers = () => {
       try {
         ipfs.swarm
           .peers()
           .then(peers => {
             console.log(peers.length + ' IPFS peers')
-            this.setState({ connected: true, peers: peers.length })
+            setPeersStatus(peers.length)
           })
-          .catch(e => this.setState({ connected: false }))
+          .catch(e => setPeersStatus(false))
       } catch (e) {
-        this.setState({ connected: false })
+        setPeersStatus(false)
       }
     }
-    this.interval = setInterval(checkPeers, 5000)
-  }
+    const interval = setInterval(checkPeers, 5000)
+    return () => clearInterval(interval)
+  })
 
-  componentWillUnmount() {
-    clearInterval(this.interval)
-  }
-
-  render() {
-    return !this.state.connected ? (
-      <div style={{ color: 'red' }}>
-        <Trans i18nKey="ipfs-not-running">
-          ⚠ Warning: IPFS daemon is not running or is not available in your
-          computer. Please{' '}
-          <SafeLink href="https://dist.ipfs.io/#go-ipfs" target="_blank">
-            <strong>install it</strong>
-          </SafeLink>{' '}
-          and run <strong>ipfs daemon</strong> on your console.
-        </Trans>
-      </div>
-    ) : this.state.peers === 0 ? (
-      <div style={{ color: 'red' }}>
-        <Trans i18nKey="ipfs-not-connected">
-          ⚠ Warning: IPFS is not connected to any peer. The content of your
-          edits is not going to be propagated. Please, check your internet
-          connection.
-        </Trans>
-      </div>
-    ) : null
-  }
+  return peers === false ? (
+    <div style={{ color: 'red' }}>
+      <Trans i18nKey="ipfs-not-running">
+        ⚠ Warning: IPFS daemon is not running or is not available in your
+        computer. Please{' '}
+        <SafeLink href="https://dist.ipfs.io/#go-ipfs" target="_blank">
+          <strong>install it</strong>
+        </SafeLink>{' '}
+        and run <strong>ipfs daemon</strong> on your console.
+      </Trans>
+    </div>
+  ) : peers === 0 ? (
+    <div style={{ color: 'red' }}>
+      <Trans i18nKey="ipfs-not-connected">
+        ⚠ Warning: IPFS is not connected to any peer. The content of your edits
+        is not going to be propagated. Please, check your internet connection.
+      </Trans>
+    </div>
+  ) : null
 }
 
 export default IpfsIsConnected
